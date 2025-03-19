97a42f2d9a7543e44f0ec00cb071766233fc47e0
CARLOS MO <carlosmongeorihuela.2003@gmail.com>
2025-03-18 23:00:45 -0500

    Guardando cambios antes de rebase

---
+++ src/main/java/com/neoinfo/neoback/NeobackApplication.java
@@ -0,0 +1,13 @@
+package com.neoinfo.neoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeobackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neoinfo/neoback/NeobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoinfo.neoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
